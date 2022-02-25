81c7ed2aeb6dca5921e05d60ea7f6545f3b31a8b
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-25 09:10:43 +0500

    Add

--- src/Box/BoxProject/Box.js
+++ src/Box/BoxProject/Box.js
@@ -5,20 +5,25 @@ import Boxx from "./Boxx";
 function Box() {
   const [box, setBox] = React.useState(boxes);
   const toggle = (id) => {
-    setBox((prev) => {
-      const newSquaare = [];
-      for (let i = 0; i < prev.length; i++) {
-        const currentValue = prev[i];
-        if (currentValue.id === id) {
-          const update = {
-            ...prev,
-            on: !prev.on,
-          };
-          newSquaare.push(update);
-        } else newSquaare.push(currentValue);
-      }
-      return newSquaare;
+    setBox((previous) => {
+      return previous.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : box;
+      });
     });
+    // setBox((prev) => {
+    //   const newSquaare = [];
+    //   for (let i = 0; i < prev.length; i++) {
+    //     const currentValue = prev[i];
+    //     if (currentValue.id === id) {
+    //       const update = {
+    //         ...prev,
+    //         on: !prev.on,
+    //       };
+    //       newSquaare.push(update);
+    //     } else newSquaare.push(currentValue);
+    //   }
+    //   return newSquaare;
+    // });
   };
 
   const allboxes = box.map((square) => (
